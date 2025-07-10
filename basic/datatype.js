c990bce64373ca88f63747b7345c62423f2d540f
anup-kumar29 <anupkumarr1029@gmail.com>
2025-07-10 18:29:54 +0000

    datatypes

---
+++ basic/datatype.js
@@ -0,0 +1,24 @@
+
+//earlier in js there was no class,module,arrow function , to make oldercode treated as newer code in standard because it is run by same engine
+//so we will use 
+
+//"use strict" //treat all code as newer version
+"use strict";
+// alert("heloow")
+// alert(3+3)//weare using node js not broweser//different syntax in node //can be used in console
+
+
+//ecma gives the standard for js
+
+
+//primitive
+//datatype :- string number boolean null(it is standalone value) symbol(to find uniqueness among component)
+//number=>2 to power 53
+//bigint
+//undefined => when a value is not defined in a variable,
+//null repreatation of empty value
+
+
+
+console.log(typeof null); //object  
+console.log(typeof undefined); //undefined: it is  one type in js 
